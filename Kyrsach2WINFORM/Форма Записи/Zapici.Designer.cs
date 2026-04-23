b27350c6f029d714b04e44b187c6d2338a122e3f
Sergey <Sergey>
2026-04-23 22:05:28 +0300

    Добавил пагинацию на форму "Записи" и скрытие персональных данных для "Оплаты" и "Записи"

--- Kyrsach2WINFORM/Форма Записи/Zapici.Designer.cs
+++ Kyrsach2WINFORM/Форма Записи/Zapici.Designer.cs
@@ -29,9 +29,9 @@ namespace Kyrsach2WINFORM
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.label3 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
@@ -42,6 +42,8 @@ namespace Kyrsach2WINFORM
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
             // 
@@ -111,7 +113,7 @@ namespace Kyrsach2WINFORM
             this.textBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.textBox2.Location = new System.Drawing.Point(84, 71);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(643, 34);
+            this.textBox2.Size = new System.Drawing.Size(794, 34);
             this.textBox2.TabIndex = 42;
             this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
@@ -129,36 +131,36 @@ namespace Kyrsach2WINFORM
             this.dataGridView2.BackgroundColor = System.Drawing.Color.White;
             this.dataGridView2.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.dataGridView2.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
-            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle1.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dataGridView2.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView2.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle4;
             this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
-            dataGridViewCellStyle2.Font = new System.Drawing.Font("Comic Sans MS", 14.25F);
-            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
-            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.dataGridView2.DefaultCellStyle = dataGridViewCellStyle2;
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle5.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Comic Sans MS", 14.25F);
+            dataGridViewCellStyle5.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView2.DefaultCellStyle = dataGridViewCellStyle5;
             this.dataGridView2.GridColor = System.Drawing.SystemColors.ControlLightLight;
             this.dataGridView2.Location = new System.Drawing.Point(11, 111);
             this.dataGridView2.MultiSelect = false;
             this.dataGridView2.Name = "dataGridView2";
             this.dataGridView2.ReadOnly = true;
-            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle3.Font = new System.Drawing.Font("Comic Sans MS", 14.25F);
-            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dataGridView2.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle6.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("Comic Sans MS", 14.25F);
+            dataGridViewCellStyle6.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView2.RowHeadersDefaultCellStyle = dataGridViewCellStyle6;
             this.dataGridView2.RowHeadersVisible = false;
             this.dataGridView2.Size = new System.Drawing.Size(1201, 419);
             this.dataGridView2.TabIndex = 41;
@@ -194,7 +196,7 @@ namespace Kyrsach2WINFORM
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Comic Sans MS", 14.25F);
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.label1.Location = new System.Drawing.Point(997, 42);
+            this.label1.Location = new System.Drawing.Point(1097, 42);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(115, 26);
             this.label1.TabIndex = 54;
@@ -229,12 +231,33 @@ namespace Kyrsach2WINFORM
             this.button3.UseVisualStyleBackColor = false;
             this.button3.Click += new System.EventHandler(this.button8_Click);
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Comic Sans MS", 14.25F);
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label4.Location = new System.Drawing.Point(79, 533);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(162, 26);
+            this.label4.TabIndex = 57;
+            this.label4.Text = "Кол-во записей:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(307, 536);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(401, 52);
+            this.groupBox1.TabIndex = 58;
+            this.groupBox1.TabStop = false;
+            // 
             // Zapici
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Bisque;
             this.ClientSize = new System.Drawing.Size(1225, 600);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -268,5 +291,7 @@ namespace Kyrsach2WINFORM
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
--- Kyrsach2WINFORM/Форма Записи/Zapici.cs
+++ Kyrsach2WINFORM/Форма Записи/Zapici.cs
@@ -9,42 +9,26 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 //
 using MySql.Data.MySqlClient;
+using VPaged.WF;
 
 namespace Kyrsach2WINFORM
 {
     public partial class Zapici : Form
     {
+        private VPagination _pag;
+
         public Zapici()
         {
             InitializeComponent();
 
+            _pag = new VPagination(this.groupBox1, pageSize: 10);
+            _pag.SelectDataMaster = FillDataGrid;
+            _pag.SelectCountMaster = GetCount;
+
+
             // Включаем двойную буферизацию для DataGridView
             Optimize.SetDoubleBuffered(dataGridView2);
             dataGridView2.CellBorderStyle = DataGridViewCellBorderStyle.None;
-
-            //Настраиваем ДатаГрид
-            dataGridView2.Columns.Add("Number", "Номер");
-            dataGridView2.Columns.Add("IDClient", "ID");
-            dataGridView2.Columns.Add("IDMaser", "ID2");
-            dataGridView2.Columns["IDClient"].Visible = false;
-            dataGridView2.Columns["IDMaser"].Visible = false;
-
-            dataGridView2.Columns.Add("FIO", "ФИО клиента");
-            dataGridView2.Columns.Add("Phone", "Телефон");
-            dataGridView2.Columns["Phone"].Visible = false;
-            dataGridView2.Columns.Add("FIO2", "ФИО мастера");
-            dataGridView2.Columns["FIO2"].Visible = false;
-            dataGridView2.Columns.Add("Status", "Статус");
-            dataGridView2.Columns.Add("Date_Record", "Дата записи");
-            dataGridView2.Columns.Add("Time_Record", "Время записи");
-            dataGridView2.Columns.Add("Totla_Price", "Сумма записи");
-            dataGridView2.Columns.Add("Total_Time", "Продолжительность, мин.");
-            dataGridView2.Columns.Add("PhoneMaster", "PhoneMaster.");
-            dataGridView2.Columns["PhoneMaster"].Visible = false;
-            dataGridView2.Columns["Total_Time"].DefaultCellStyle.Padding = new Padding(0, 5, 0, 5);
-
-            foreach (DataGridViewColumn column in dataGridView2.Columns)
-                column.MinimumWidth = 100;
 
             //Сортировка по дате
             comboBox1.Items.Add("По умолчанию");
@@ -62,6 +46,8 @@ namespace Kyrsach2WINFORM
             }
         }
 
+        
+
         int CurrentRowIndex; // Индекс выбранной строки
         // Получаем инфу по выбранной строке ДатаГрида
         private void dataGridView2_CellClick(object sender, DataGridViewCellEventArgs e)
@@ -75,7 +61,7 @@ namespace Kyrsach2WINFORM
                 dataGridView2.ClearSelection();
                 return;
             }       // Если строка в статусе "Завершен" запрещяем редактирование и удаление
-            else if (dataGridView2.Rows[CurrentRowIndex].Cells["Status"].Value.ToString() == "Завершен")
+            else if (dataGridView2.Rows[CurrentRowIndex].Cells["Статус"].Value.ToString() == "Завершен")
             {
                 button1.Enabled = false;
                 button6.Enabled = false;
@@ -86,43 +72,69 @@ namespace Kyrsach2WINFORM
             button6.Enabled = true;
         }
 
-        // Загрузка формы - очищяем выделение
-        private void Zapici_Load(object sender, EventArgs e)
-        {
-            dataGridView2.ClearSelection();
-        }
-
-        //Сортировка - очищяем выделение
-        private void dataGridView2_Sorted(object sender, EventArgs e)
-        {
-            dataGridView2.ClearSelection();
-        }
-
-        // Заполняет данными таблицу
-        void FillDataGrid(string CMDString)
+        //Для подсчета количества записей
+        long countRow = 0;
+        private long GetCount()
         {
             try
             {
+                // Общий запрос без LIMIT
+                string countSql = $"SELECT COUNT(*) FROM ({CMD} {having}) AS t";
+
+                using (MySqlConnection Con = new MySqlConnection(ConnectAndData.Сonnect))
+                {
+                    Con.Open();
+                    using (MySqlCommand cmd = new MySqlCommand(countSql, Con))
+                    {
+                        object result = cmd.ExecuteScalar();
+                        return countRow = Convert.ToInt64(result ?? 0);
+                    }
+                }
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+
+        // Заполняет данными таблицу
+        void FillDataGrid()
+        {
+            try
+            {
+                dataGridView2.DataSource = null;
+                dataGridView2.Columns.Clear();
+
+                // Формируем пагинированный SQL
+                int skip = (_pag.PageIndex - 1) * _pag.PageSize;
+                string fullSql = CMD + having + orderBy +
+                    $" LIMIT {skip}, {_pag.PageSize}";
+
                 using (MySqlConnection Con = new MySqlConnection(ConnectAndData.Сonnect))
                 {
                     Con.Open();
 
-                    MySqlCommand cmd = new MySqlCommand(CMDString, Con);
-                    MySqlDataReader RDR = cmd.ExecuteReader();
+                    MySqlCommand cmd = new MySqlCommand(fullSql, Con);
+                    MySqlDataAdapter ad = new MySqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
 
+                    
+                    ad.Fill(dt);
+                   
+                    dataGridView2.DataSource = dt;
+                    label4.Text = $"Кол-во записей: {countRow}";
 
-                    //dataGridView2.Columns.Clear();
+                    dataGridView2.Columns["Продолжительность, мин."].DefaultCellStyle.Padding = new Padding(0, 5, 0, 5);
 
-                    dataGridView2.Rows.Clear();
-                    //Заполняем данными
-                    while (RDR.Read())
-                    {
-                        // Форматируем данные связанный со временем
-                        string Date = (DateTime.Parse(RDR[7].ToString())).ToString("dd.MM.yyyy");
-                        string Time = (DateTime.Parse(RDR[8].ToString())).ToString("HH:mm");
+                    dataGridView2.Columns["Телефон"].Visible = false;
+                    dataGridView2.Columns["IDClient"].Visible = false;
+                    dataGridView2.Columns["IDMaser"].Visible = false;
+                    dataGridView2.Columns["ФИО мастера"].Visible = false;
+                    dataGridView2.Columns["PhoneMaster"].Visible = false;
 
-                        dataGridView2.Rows.Add(RDR[0].ToString(), RDR[1].ToString(), RDR[2].ToString(), RDR[3].ToString(), RDR[4].ToString(), RDR[5].ToString(), RDR[6].ToString(), Date, Time, RDR[9].ToString(), RDR[10].ToString(), RDR[11].ToString());
-                    }
+                    foreach (DataGridViewColumn column in dataGridView2.Columns)
+                        column.MinimumWidth = 100;
+
 
                     dataGridView2.ClearSelection(); //Очистка выделения
                     button1.Enabled = false;
@@ -139,18 +151,18 @@ namespace Kyrsach2WINFORM
         private void button1_Click(object sender, EventArgs e)
         {
             //Формируем нашу запись в объект
-            string ID = dataGridView2.Rows[CurrentRowIndex].Cells["Number"].Value.ToString();
+            string ID = dataGridView2.Rows[CurrentRowIndex].Cells["Номер"].Value.ToString();
             string IDClient = dataGridView2.Rows[CurrentRowIndex].Cells["IDClient"].Value.ToString();
             string IdMaster = dataGridView2.Rows[CurrentRowIndex].Cells["IDMaser"].Value.ToString();
 
-            string FIOClient = dataGridView2.Rows[CurrentRowIndex].Cells["FIO"].Value.ToString();
-            string FIOMaster = dataGridView2.Rows[CurrentRowIndex].Cells["FIO2"].Value.ToString();
-            string Phone = dataGridView2.Rows[CurrentRowIndex].Cells["Phone"].Value.ToString();
-            string Status = dataGridView2.Rows[CurrentRowIndex].Cells["Status"].Value.ToString();
-            string Time = dataGridView2.Rows[CurrentRowIndex].Cells["Time_Record"].Value.ToString();
-            string Dration = dataGridView2.Rows[CurrentRowIndex].Cells["Total_Time"].Value.ToString(); 
-            string Price = dataGridView2.Rows[CurrentRowIndex].Cells["Totla_Price"].Value.ToString();
-            string Date = dataGridView2.Rows[CurrentRowIndex].Cells["Date_Record"].Value.ToString();
+            string FIOClient = dataGridView2.Rows[CurrentRowIndex].Cells["ФИО клиента"].Value.ToString();
+            string FIOMaster = dataGridView2.Rows[CurrentRowIndex].Cells["ФИО мастера"].Value.ToString();
+            string Phone = dataGridView2.Rows[CurrentRowIndex].Cells["Телефон"].Value.ToString();
+            string Status = dataGridView2.Rows[CurrentRowIndex].Cells["Статус"].Value.ToString();
+            string Time = dataGridView2.Rows[CurrentRowIndex].Cells["Время записи"].Value.ToString();
+            string Dration = dataGridView2.Rows[CurrentRowIndex].Cells["Продолжительность, мин."].Value.ToString(); 
+            string Price = dataGridView2.Rows[CurrentRowIndex].Cells["Сумма записи"].Value.ToString();
+            string Date = dataGridView2.Rows[CurrentRowIndex].Cells["Дата записи"].Value.ToString();
 
             string PhoneMaster = dataGridView2.Rows[CurrentRowIndex].Cells["PhoneMaster"].Value.ToString();
 
@@ -164,7 +176,7 @@ namespace Kyrsach2WINFORM
         //Удаление
         private void deleteOrder_Click(object sender, EventArgs e)
         {
-            string ID = dataGridView2.Rows[CurrentRowIndex].Cells["Number"].Value.ToString();
+            string ID = dataGridView2.Rows[CurrentRowIndex].Cells["Номер"].Value.ToString();
 
             //2 запроса - Одна транзакция
             MySqlTransaction transaction = null;
@@ -219,78 +231,69 @@ namespace Kyrsach2WINFORM
             textBox2_TextChanged(textBox2, EventArgs.Empty);
         }
 
+
+
         // Строка запроса default
-        string CMD = @"SELECT IdRecord, Client.IdClient, Employe.IdEmploye,
-                CONCAT_WS(' ', Client.Name, Client.Surname, Client.Patronymic) AS 'ФИОк', 
-                Client.Phone,
-                CONCAT_WS(' ', Employe.Name, Employe.Surname, Employe.Patronymic) AS 'ФИОм',  
-                Status.Name, Date_Record, Time_Record, Totla_Price, Total_Time, Employe.Phone FROM Record 
+        string CMD = @"SELECT IdRecord as 'Номер', Client.IdClient as 'IDClient', Employe.IdEmploye 'IDMaser',
+                CONCAT_WS(' ', Client.Name, Client.Surname, Client.Patronymic) AS 'ФИО клиента', 
+                Client.Phone as 'Телефон',
+                CONCAT_WS(' ', Employe.Name, Employe.Surname, Employe.Patronymic) AS 'ФИО мастера',  
+                Status.Name as 'Статус', 
+
+                DATE_FORMAT(Date_Record, '%d.%m.%Y') AS 'Дата записи',  -- Формат dd.MM.yyyy
+                DATE_FORMAT(Time_Record, '%H:%i') AS 'Время записи',    -- Формат HH:MM
+                
+                Totla_Price as 'Сумма записи', Total_Time as 'Продолжительность, мин.', Employe.Phone as 'PhoneMaster' FROM Record 
 
                     INNER JOIN `Client` ON IdClient = Id_Client 
                     INNER JOIN `Employe` ON IdEmploye = Id_Employe  
                     INNER JOIN `Status` ON IdStatus = Id_Status";
 
+        string having = "";
+        string orderBy = "";
 
-        void SendRequst()
-        {
-            int I = comboBox1.SelectedIndex;
 
-            switch (I)
-            {
-                case 0:
-                    FillDataGrid(CMD + $" WHERE IdRecord LIKE '%{textBox2.Text}%';");
-                    break;
-                case 1:
-                    FillDataGrid(CMD + $" WHERE IdRecord LIKE '%{textBox2.Text}%' ORDER BY Date_Record {SortMod};");
-                    break;
-                case 2:
-                    FillDataGrid(CMD + $" WHERE IdRecord LIKE '%{textBox2.Text}%' ORDER BY Date_Record {SortMod};");
-                    break;
-            }
-        }
 
-        //СОРТИРОВКА
-        //СОРТИРОВКА
         //СОРТИРОВКА  (по дате)
-        string SortMod;
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            switch (comboBox1.SelectedIndex)
-            {
-                case 1:
-                    SortMod = "ASC";         // по возрастанию
-                    break;
-
-                case 2:
-                    SortMod = "DESC";        // по убыванию
-                    break;
-            }
-
-            SendRequst();
+            if (comboBox1.SelectedIndex == 0)
+                orderBy = $" ORDER BY IdRecord ASC";
+            else if(comboBox1.SelectedIndex == 1)
+                orderBy = $" ORDER BY Date_Record";
+            else
+                orderBy = $" ORDER BY Date_Record DESC";
+            FillDataGrid();
+            _pag.VPagRunOrRefresh();
         }
 
         // "Живой" поиск (по номеру)
         private void textBox2_TextChanged(object sender, EventArgs e)
         {
-            SendRequst();
+            if (textBox2.Text.Length == 0)
+                having = "";
+            else
+                having = $" HAVING IdRecord LIKE '%{textBox2.Text}%'";
+            FillDataGrid();
+            _pag.VPagRunOrRefresh();
         }
 
 
         //Прячем некоторые данные
         private void dataGridView2_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
         {
-            if (dataGridView2.Columns[e.ColumnIndex].Name == "FIO")
-            {
-                if (e.Value != null)
+            if (ShowText)
+            {// делаем так, чтобы раскрывалась только та строка, на которую указали мышкой
+                if (dataGridView2.Columns[e.ColumnIndex].Name == "ФИО клиента" && e.RowIndex != ThisRow)
                 {
-                    var Val = e.Value.ToString().Split(' ');
-                    string Result;
-                    if (Val.Length == 3 && Val[2].Trim() != "")
-                        Result = Val[0] + " " + (Val[1])[0] + "." + " " + (Val[2])[0] + ".";
-                    else
-                        Result = Val[0] + " " + (Val[1])[0] + ".";
-
-                    e.Value = Result;
+                    Optimize.HideMyFio(e);
+                }
+            }
+            else
+            {// прячем все
+                if (dataGridView2.Columns[e.ColumnIndex].Name == "ФИО клиента")
+                {
+                    Optimize.HideMyFio(e);
                 }
             }
         }
@@ -306,24 +309,57 @@ namespace Kyrsach2WINFORM
         }
 
 
-        //Закрыть
-        private void button8_Click(object sender, EventArgs e)
-        {
-            MenuAdmin.DisableButton();
-            this.Close();
-        }
+
+        //ВЫДЕЛЕНИЕ СТРОКИ
+
+        bool ShowText = false;
+        int ThisRow;
 
         //Подсветка строки на которую направлен указатель мыши
         private void dataGridView2_CellMouseMove(object sender, DataGridViewCellMouseEventArgs e)
         {
             if (e.RowIndex > -1)
+            {
                 dataGridView2.Rows[e.RowIndex].DefaultCellStyle.BackColor = Color.LightGray;
+                ShowText = true;
+                ThisRow = e.RowIndex;
+                dataGridView2.Rows[e.RowIndex].Cells["ФИО клиента"].Value = dataGridView2.Rows[e.RowIndex].Cells["ФИО клиента"].Value;
+            }
+               
         }
         //Возвращаем состояние строки на исходную, когда указатель "Покидает" строку
         private void dataGridView2_CellMouseLeave(object sender, DataGridViewCellEventArgs e)
         {
             if (e.RowIndex > -1)
+            {
                 dataGridView2.Rows[e.RowIndex].DefaultCellStyle.BackColor = Color.White;
+                ShowText = false;
+                dataGridView2.Rows[e.RowIndex].Cells["ФИО клиента"].Value = dataGridView2.Rows[e.RowIndex].Cells["ФИО клиента"].Value;
+            }
+                
+        }
+
+        //Установил Nuget пакет для пагинации, VPaged.WF + System.Windows.Forms
+
+
+        // Загрузка формы - очищяем выделение
+        private void Zapici_Load(object sender, EventArgs e)
+        {
+            dataGridView2.ClearSelection();
+            _pag.VPagRunOrRefresh();
+        }
+
+        //Сортировка - очищяем выделение
+        private void dataGridView2_Sorted(object sender, EventArgs e)
+        {
+            dataGridView2.ClearSelection();
+        }
+
+        //Закрыть
+        private void button8_Click(object sender, EventArgs e)
+        {
+            MenuAdmin.DisableButton();
+            this.Close();
         }
     }
 }
